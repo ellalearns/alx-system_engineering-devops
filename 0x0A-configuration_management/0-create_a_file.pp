a6d57dfe1039b54104212033e6e3a85f196be0c8
ellalearns <ellalearnscopy@gmail.com>
2024-05-07 06:29:22 -0700

    task 0 :

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,11 @@
+#!/usr/bin/pup
+# create a file in /tmp with Puppet
+
+file {'/tmp/school':
+ensure => present,
+mode => '0744',
+owner => 'www-data',
+group => 'www-data',
+content => 'I love Puppet'
+}
+
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,3 @@
+## Configuration Management Tools
+Using Puppet to configure servers -- create file, install package, kill process
+
