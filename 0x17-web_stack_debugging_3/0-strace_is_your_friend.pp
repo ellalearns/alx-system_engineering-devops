606a95e851be7106d97b38925667423d4747aa8e
ellalearns <ellalearnscopy@gmail.com>
2024-06-21 04:23:06 +0100

    fix issue

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,10 @@
+# solves a 500 server error
+# replaces a faulty line in a filee
+# changes 'phpp' to 'php'
+
+
+$file_name = /var/www/html/wp-settings.php
+exec { 'replace-phpp-with-php':
+    command => "sed -i 's/phpp/php/g' $file_name"
+    path    => "/usr/local/bin/:/bin/"
+}
