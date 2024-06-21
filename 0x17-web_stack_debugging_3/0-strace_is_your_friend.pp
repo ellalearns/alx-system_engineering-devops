55bb74d8484b16c8920b357c0c5b7ea769d2b4a3
ellalearns <ellalearnscopy@gmail.com>
2024-06-21 04:28:27 +0100

    mods

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,8 +2,8 @@
 # replaces a faulty line in a filee
 # changes 'phpp' to 'php'
 
+$file_name = '/var/www/html/wp-settings.php'
 
-$file_name = /var/www/html/wp-settings.php
 exec { 'replace-phpp-with-php':
     command => "sed -i 's/phpp/php/g' $file_name"
     path    => "/usr/local/bin/:/bin/"
