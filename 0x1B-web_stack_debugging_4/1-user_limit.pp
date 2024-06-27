2e86bf63b74c231ca79880440f45b9ba178e7c23
ellalearns <ellalearnscopy@gmail.com>
2024-06-27 23:47:35 +0100

    user limits

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,16 @@
+# allow user holberton access to nginx server
+
+$user_soft_file = '/holberton soft/s/4/70000'
+$user_hard_file = '/holberton soft/s/5/70000/'
+$file_name = '/etc/security/limits/conf'
+$path = '/usr/local/bin/:/bin/'
+
+exec { 'increase-hard-file-access-for-holberton'
+    command => 'sed -i ${user_hard_file} ${file_name},
+    path    => ${path}
+}
+
+exec { 'increase-soft-file-access-for-holberton'
+    command => 'sed -i ${user_soft_file} ${file_name},
+    path    => ${path}
+}
