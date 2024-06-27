9185906694e9c1d9d13b5ba02cf6ca6beccfba1c
ellalearns <ellalearnscopy@gmail.com>
2024-06-28 00:05:43 +0100

    user limits

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -5,11 +5,11 @@ $user_hard_file = '/holberton hard/s/5/50000/'
 $file_name = '/etc/security/limits.conf'
 
 exec { 'increase-hard-file-access-for-holberton':
-    command => "sed -i ${user_hard_file} ${file_name}",
+    command => "echo '${user_hard_file}' >> ${file_name}",
     path    => '/usr/local/bin/:/bin/'
 }
 
 exec { 'increase-soft-file-access-for-holberton':
-    command => "sed -i ${user_soft_file} ${file_name}",
+    command => "echo '${user_soft_file}' >> ${file_name}",
     path    => '/usr/local/bin/:/bin/'
 }
