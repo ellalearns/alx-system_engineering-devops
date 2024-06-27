170cd70d9f46a3ef33ede108c7cf07852dfe1720
ellalearns <ellalearnscopy@gmail.com>
2024-06-27 22:59:50 +0100

    increase traffic limit mods

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,17 +2,17 @@
 # This file fixes it by modifying the open file limit
 
 $file_name = '/etc/default/nginx'
-$com_seq = 's/15/4096/'
+$com_seq   = 's/15/4096/'
 
 # fix nginx traffic limit
 exec { 'increase-traffic-limit':
-command => "sed -i ${com_seq} ${file_name}",
-path    => '/usr/local/bin/:/bin/'
+    command => "sed -i ${com_seq} ${file_name}",
+    path    => '/usr/local/bin/:/bin/'
 } ->
 
 # restart nginx
 exec {'restart-nginx':
-command => 'nginx restart',
-path    => '/etc/init.d/'
+    command => 'nginx restart',
+    path    => '/etc/init.d/'
 }
 
